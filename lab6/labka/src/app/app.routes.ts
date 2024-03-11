98cf616b8d68c739e13423e6dd53ac637df915dd
Nelle <nurievaaa28@gmail.com>
2024-03-12 01:29:23 +0600

    init commit

--- lab6/labka/src/app/app.routes.ts
+++ lab6/labka/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent}
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent, title: 'Home'},
+    {path: 'about', component: AboutComponent, title: 'About'},
+    {path: '**', component: NotFoundComponent, title: '404 Not Found'}
 
 ];
