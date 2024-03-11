3444d306ce90b1750cbe29b35d22de12bdf3078c
Nelle <nurievaaa28@gmail.com>
2024-03-12 01:20:19 +0600

    init commit

--- lab6/labka/src/app/app.component.ts
+++ lab6/labka/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
