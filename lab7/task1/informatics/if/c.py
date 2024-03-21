7f47b3c81aa0f86a09dbe3b641a38223b5147bd6
Nelle <nurievaaa28@gmail.com>
2024-03-21 17:26:05 +0600

    init commit

---
+++ lab7/task1/informatics/if/c.py
@@ -0,0 +1,13 @@
+system_answer = int(input())
+student_answer = int(input())
+
+if system_answer == 1:
+    if student_answer == 1:
+        print("YES")
+    else:
+        print("NO")
+else:
+    if student_answer == 1:
+        print("NO")
+    else:
+        print("YES")
