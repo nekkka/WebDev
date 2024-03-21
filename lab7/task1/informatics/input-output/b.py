52eaee0246f937d114b2a97fb52624d8759f0b51
Nelle <nurievaaa28@gmail.com>
2024-03-21 16:55:50 +0600

    init commit

---
+++ lab7/task1/informatics/input-output/b.py
@@ -0,0 +1,4 @@
+a = int(input())
+
+print('The next number for the number {a} is {more}'.format(a = a, more = a + 1))
+print('The previous number for the number {a} is {less}'.format(a = a, less = a - 1))
